3ed0cbc4d7c60a21f76d0d9267d6d78531234c7e
Cam <camglowicki@hotmail.com>
2021-01-19 15:06:32 -0800

    mapped over days array to retureve daylist item components

---
+++ src/components/DayList.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+
+  //key can also = day.id
+  let days = props.days.map((day, i) => {
+    return (
+      <DayListItem
+        key = {i}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay} />
+    )
+  });
+
+  return (
+    <ul>
+      {days}
+    </ul>
+  );
+}
