bc98f8ac6ee07c338832b09ffd7b31b6c38f93ed
Cam <camglowicki@hotmail.com>
2021-01-19 18:48:59 -0800

    built interviewer item and styled

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import "components/InterviewerListItem.scss"
+
+const classNames = require('classnames');
+
+export default function InterviewerListItem(props) {
+
+  const className = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  })
+  return (
+    <li className={className} onClick={props.setInterviewer}>
+  <img
+    className="interviewers__item-image"
+    src={props.avatar}
+    alt={props.name}
+  />
+  {props.name}
+</li>
+  );
+};
