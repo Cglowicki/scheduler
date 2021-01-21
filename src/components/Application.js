52adf1ac4408ed8156bb2ce00358f1a673746d4b
Cam <camglowicki@hotmail.com>
2021-01-20 21:58:44 -0800

    displaying appointments, adding fake data to flesh out the browser in Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import DayList from "components/DayList";
+import Appointment from "components/Appointment"
 
 import "components/Application.scss";
 
@@ -22,8 +23,103 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+
+  {
+    id: 2,
+    time: "2pm",
+  },
+
+  {
+    id: 3,
+    time: "3pm",
+    interview: {
+      student: "Patricia Flowers",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+
+  {
+    id: 3,
+    time: "4pm",
+  },
+
+  {
+    id: 4,
+    time: "5pm",
+    interview: {
+      student: "Leroy Jenkins",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  },
+
+  {
+    id: 4,
+    time: "6pm",
+  },
+
+  {
+    id: 5,
+    time: "7pm",
+    interview: {
+      student: "Sally Withers",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcom",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  }
+];
+
+/*   let interviewers = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={(event) => props.setInterviewer(interviewer.id)}
+      />
+    );
+  }); */
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+
+  let appointment = appointments.map(appointment => {
+    return (
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+      />
+    );
+  });
 
   return (
     <main className="layout">
@@ -48,7 +144,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointment}
+        <Appointment key="last" time="8pm" />
       </section>
     </main>
   );
